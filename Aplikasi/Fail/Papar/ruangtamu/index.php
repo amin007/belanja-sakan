e3c84b1a68db044bd5ebc8f79dd94e4ccae4fc76
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-17 13:58:27 +0800

    tukar pautan baru

--- Aplikasi/Fail/Papar/ruangtamu/index.php
+++ Aplikasi/Fail/Papar/ruangtamu/index.php
@@ -7,13 +7,15 @@ $Sesi->init();
 //echo '<pre>'; print_r($_SESSION) . '</pre>';
 echo 'namaPendek=' . $Sesi->get('be18_namaPendek') . '<br>';
 echo 'namaPenuh=' . $Sesi->get('be18_namaPenuh') . '<br>';
-echo 'levelPengguna=' . $Sesi->get('be18_levelPengguna') . '';//*/
+echo 'levelPengguna=' . $Sesi->get('be18_levelPengguna') . '<br>';
+echo 'url=' . URL . '';
+//*/
 ?></p>
 
 	<a class="btn btn-primary btn-large" href="<?php echo URL ?>ruangtamu/logout">Pergi Lebih Jauh<i class="fa fa-binoculars fa-2x"></i></a>
-	<a class="btn btn-success btn-large" target="_blank" href="<?php echo URL ?>borang">Borang<i class="fa fa-binoculars fa-2x"></i></a>
-	<a class="btn btn-success btn-large" target="_blank" href="<?php echo URL ?>borang/industri">MSIC<i class="fa fa-binoculars fa-2x"></i></a>
-	<a class="btn btn-warning btn-large" target="_blank" href="<?php echo URL ?>qss">QSS<i class="fa fa-binoculars fa-2x"></i></a>
+	<a class="btn btn-success btn-large" target="_blank" href="<?php echo URL ?>dapatan">Dapatan<i class="fa fa-binoculars fa-2x"></i></a>
+	<a class="btn btn-danger btn-large" target="_blank" href="<?php echo URL ?>belian">Belian<i class="fa fa-binoculars fa-2x"></i></a>
+	<a class="btn btn-warning btn-large" target="_blank" href="<?php echo URL ?>semakan">semakan<i class="fa fa-binoculars fa-2x"></i></a>
 
 </div><!-- / class="hero-unit" -->
 </div><!-- / class="container" -->
