0d0faa2892f1e9216700dc297b8b24c54345f064
Amin Ledang @ Dell <al.amin007@yahoo.com>
2018-12-19 01:50:18 +0800

    buang fungsi terlibat

--- Aplikasi/Kelas/Kitab/Vendor/Borang/Html_Input_Biasa.php
+++ Aplikasi/Kelas/Kitab/Vendor/Borang/Html_Input_Biasa.php
@@ -27,33 +27,6 @@ class Html_Input
 		return $input2;
 	}
 #------------------------------------------------------------------------------------------
-	public static function labelBawah($labelDibawah)
-	{
-		$input2 = null;
-		$tab2 = "\n\t\t";
-		$input2 = ($labelDibawah==null) ? '' : 
-				'<span class="input-group-addon">' 
-				. $labelDibawah . '</span>'
-				. $tab2;
-
-		return $input2;
-	}
-#------------------------------------------------------------------------------------------
-	public static function labelPre($labelDibawah)
-	{
-		$input2 = null;
-		$tab2 = "\n\t\t";
-		$pre = 'pre';
-		//$pre = 'blockquote';
-		$input2 = ($labelDibawah==null) ? '' : 
-				'<' . $pre . '>' 
-				. $labelDibawah 
-				. '</' . $pre . '>'
-				. $tab2;
-
-		return $input2;
-	}
-#------------------------------------------------------------------------------------------
 	public function medanCarian($pindah, $class = 'col-sm-7')
 	{
 		list($myTable, $senarai, $cariID, $_jadual) = $pindah;
@@ -112,64 +85,6 @@ class Html_Input
 </div><?php
 	}
 #------------------------------------------------------------------------------------------
-	public function addInput($class, $myTable, $kunciUtama, $jenisMedan, $namaMedan, $data)
-	{
-		# istihar pembolehubah 
-		$labelDibawah = $data;
-		$name = 'name="' . $myTable . '[' . $namaMedan . ']"';
-		$tab2 = "\n\t\t";
-		$tab3 = "\n\t\t\t";
-		$tab4 = "\n\t\t\t\t";
-		# butang
-		$birutua = 'btn btn-primary btn-mini';
-		$birumuda = 'btn btn-info btn-mini';
-		$merah = 'btn btn-danger btn-mini';
-		$classInput = $class . 'input-group input-group';
-		$komen = '<!-- / "' . $class . 'input-group input-group" -->';
-
-		if(in_array($kunciUtama,array('primaryKey')))
-		{#untuk medan primary key
-			$data = null;
-			$input = $tab2 . '<div class="'.$classInput.'">' . $tab3
-				   //. '<span class="input-group-addon"></span>' . $tab3
-				   . $this->labelBawah($labelDibawah)
-				   . '</div>' . $komen
-				   . '';
-		}
-		elseif(in_array($jenisMedan,array('STRING')))
-		{#kod utk input text 
-			$input = $tab2 . '<div class="'.$classInput.'">' . $tab3
-				   //. '<span class="input-group-addon"></span>' . $tab3
-				   . '<input type="text" ' . $name . ' class="form-control">' . $tab3
-				   . $this->labelBawah($labelDibawah)
-				   . '</div>' . $komen
-				   . '';
-		}
-		elseif(in_array($namaMedan,array('BLOB')))
-		{#senarai medan untuk textarea
-			//$data = null;
-			$input = $tab2 . '<div class="'.$classInput.'">' . $tab3
-				   . '<textarea ' . $name . ' rows="5" cols="20"' . $tab3
-				   . ' class="form-control">' . $data . '</textarea>' . $tab3 
-				   . $this->labelPre($labelDibawah)
-				   . '</div>' . $komen . $tab3 
-				   . '';
-		}
-		elseif ( in_array($jenisMedan,array('blockquote')) )
-		{#kod untuk blockquote
-			$data = null;
-			$input = '<blockquote>'
-				   . '<p class="form-control-static text-info">' . $data . '</p>'
-				   //. '<small>Alamat <cite title="Source Title">baru</cite></small>'
-				   . '</blockquote>';
-		}
-		else
-		{#kod untuk lain2
-			$input = '<p class="form-control-static text-info">' . $data . '</p>';
-		}
-		
-		return $input; # pulangkan nilai
-	}
 #------------------------------------------------------------------------------------------
 #==========================================================================================
 }
