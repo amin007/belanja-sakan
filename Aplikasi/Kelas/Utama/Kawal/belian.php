8c1f2a65142adcf358fc882733fab3da54064d30
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-19 09:58:23 +0800

    tambah garisan

--- Aplikasi/Kelas/Utama/Kawal/belian.php
+++ Aplikasi/Kelas/Utama/Kawal/belian.php
@@ -70,7 +70,7 @@ class Belian extends \Aplikasi\Kitab\Kawal
 		endforeach;
 
 		echo '<hr><font color="red">tidak wujud : '; print_r($takWujud);
-		echo '</font>';
+		echo '</font><hr>';
 		echo '</pre>';
 	}
 #-------------------------------------------------------------------------------------------
