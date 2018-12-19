3cae26165da3b22f9f3f7f8eb88fd22aa7873a19
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-19 11:58:25 +0800

    susun data yang ada

--- Aplikasi/Fail/Papar/cari/pilih_ubah_medan00.php
+++ Aplikasi/Fail/Papar/cari/pilih_ubah_medan00.php
@@ -1,20 +1,33 @@
 	<table class="excel">
 	<?php
 	$class = $a = $b = null;
-	$html = new \Aplikasi\Kitab\HTML_Input_Biasa();
+	$printed_headers = false; # mula bina jadual
+	#-----------------------------------------------------------------
 	for ($kira=0; $kira < count($row); $kira++)
-	{## papar data $row ------------------------------------------------
-	?><tbody><?php
+	{
+		if ( !$printed_headers ) # papar tajuk medan sekali sahaja:
+		{
+			?><thead><tr><th>#</th><?php
+			foreach ( array_keys($row[$kira]) as $tajuk )
+			{
+				?><th><?php echo $tajuk ?></th><?php
+			}
+			?></tr></thead>
+	<?php	$printed_headers = true;
+		}
+	# papar data $row ------------------------------------------------
+	?><tbody>
+	<tr><td align="center"><?php echo $kira+1 ?></td><?php
+		$html = new \Aplikasi\Kitab\Html_TD;
+		$html2 = new \Aplikasi\Kitab\HTML_Input_Biasa();
 		foreach ( $row[$kira] as $key=>$data )
 		{
-			?><tr><td align="right"><?php echo $key ?></td><?php
-			$paparData = $html->addInput($class, $myTable,
+			$html->paparURL($key, $data, $myTable,$this->c1, $this->c2);
+			$paparData = $html2->addInput($class, $myTable,
 			$a, $b, $key, $data);
-			?><td><?php echo $paparData ?></td><?php
-			?><td><?php echo $data ?></td><?php
-			?></tr><?php echo "\n\t";
-		}
-		?></tbody>
+		}	//echo '<td>' . $paparData . '</td>';
+		?></tr>
+	</tbody>
 	<?php
 	}#-----------------------------------------------------------------
 	?></table>
