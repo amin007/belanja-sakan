474f655905e324035eea7c312370f1c126430cf8
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-19 12:30:51 +0800

    tukar nama table

--- Aplikasi/Kelas/Utama/Kawal/belian.php
+++ Aplikasi/Kelas/Utama/Kawal/belian.php
@@ -270,8 +270,8 @@ class Belian extends \Aplikasi\Kitab\Kawal
 		# Set pembolehubah utama
 		$this->panggilKhas01('kod_puncapembelian',null);
 		$this->panggilKhas01('kod_mediumpembayaran',null);
-		$this->pilihMedan('senarai_belanja');
-		//$this->pilihMedan('nama_pengguna');
+		//$this->pilihMedan('senarai_belanja');
+		$this->pilihMedan('nama_pengguna');
 		//$this->pilihMedan('kod_mediumpembayaran');
 		//$this->pilihMedan('kod_puncapembelian');
 		//$this->ujian01('senarai_belanja');
