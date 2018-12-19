d6062faaf1a14d9981569ea2064ae71d20758ec8
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-19 11:21:45 +0800

    tambah $html

--- Aplikasi/Fail/Papar/cari/pilih_ubah_medan00.php
+++ Aplikasi/Fail/Papar/cari/pilih_ubah_medan00.php
@@ -1,13 +1,15 @@
 	<table class="excel">
 	<?php
+	$class = $a = $b = null;
+	$html = new \Aplikasi\Kitab\HTML_Input_Biasa();
 	for ($kira=0; $kira < count($row); $kira++)
 	{## papar data $row ------------------------------------------------
 	?><tbody><?php
 		foreach ( $row[$kira] as $key=>$data )
 		{
 			?><tr><td align="right"><?php echo $key ?></td><?php
-			$paparData = $html->tambahDropInput($this->_paparMedan, $this->_j2,
-			$myTable, $kira, $key, $data);
+			$paparData = $html->addInput($class, $myTable,
+			$a, $b, $key, $data);
 			?><td><?php echo $paparData ?></td><?php
 			?><td><?php echo $data ?></td><?php
 			?></tr><?php echo "\n\t";
