eca6520152b32bfcbc0440e6d0344ced2a0c9b4f
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-20 10:10:48 +0800

    semak nilai $meta

--- Aplikasi/Fail/Papar/cari/b_baru.php
+++ Aplikasi/Fail/Papar/cari/b_baru.php
@@ -8,10 +8,11 @@ $pilihJadual = 'baru_medan02'; # borang ubah berasaskan bootstrap
 # untuk kod baru
 //echo '<pre>$carian='; print_r($this->carian); echo '</pre>';
 //echo '<pre>$senarai='; print_r($this->senarai); echo '</pre>';
-
 //echo 'template = ' . $this->template . "\r<hr>";//*/
 
 //if(!isset($this->cariID))
 //	echo '<h1>data kosong daa</h1>';
 //else # jenis template
 	include $this->template . '.php';
+#---------------------------------------------------------------------------------------
+echo '<pre>$meta='; print_r($this->_meta); echo '</pre>';
