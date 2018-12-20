b7d0ab4ed0fcb99c8ec4ca8f1346db305aa0c2d9
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-20 08:22:58 +0800

    ringkaskan koding

--- Aplikasi/Fail/Papar/cari/b_baru.php
+++ Aplikasi/Fail/Papar/cari/b_baru.php
@@ -3,21 +3,13 @@
 //$pilihJadual = 'jadual_am';
 //$pilihJadual = 'baru_medan00'; # borang baru berasaskan table
 //$pilihJadual = 'baru_medan01'; # borang baru berasaskan bootstrap
-//$pilihJadual = 'baru_medan02'; # borang ubah berasaskan bootstrap
+$pilihJadual = 'baru_medan02'; # borang ubah berasaskan bootstrap
 
 # untuk kod baru
 //echo '<pre>$carian='; print_r($this->carian); echo '</pre>';
 //echo '<pre>$senarai='; print_r($this->senarai); echo '</pre>';
 
-# papar hasil carian
-$cari1 = '&nbsp;|&nbsp;'; $cari2 = '';
-foreach ($this->carian as $kunci => $nilai)
-	$cari1 .= ( count($this->carian)==0 ) ? $nilai : $nilai . ' | ';
-foreach ($this->senarai as $kunci2 => $nilai2)
-	$cari2 .= ( count($nilai2)==0 ) ? $kunci2 . " = Kosong<br>\r"
-	: $kunci2 . ' = ' . count($nilai2) . "<br>\r";
-echo "Anda mencari = $cari1\r<br>$cari2\r<hr>";//*/
-echo 'template = ' . $this->template . "\r<hr>";//*/
+//echo 'template = ' . $this->template . "\r<hr>";//*/
 
 //if(!isset($this->cariID))
 //	echo '<h1>data kosong daa</h1>';
