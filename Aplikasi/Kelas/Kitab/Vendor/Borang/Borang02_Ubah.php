60fa8b9cc56839c9afbdb310bccf513954127316
Amin Ledang @ Dell <helmi007@yahoo.com>
2018-12-22 14:13:35 +0800

    buang koding lama

--- Aplikasi/Kelas/Kitab/Vendor/Borang/Borang02_Ubah.php
+++ Aplikasi/Kelas/Kitab/Vendor/Borang/Borang02_Ubah.php
@@ -152,34 +152,6 @@ class Borang02_Ubah
 			$input = $this->inputNumber($semua);
 		elseif(in_array($dataType,array('NEWDECIMAL')))
 			$input = $this->inputMataDuitan($semua);
-		/*elseif ( in_array($key,array('password','kataLaluan')) )
-			$input = $this->inputPassword($tab2, $tab3, $name, $data,
-				$classInput, $komenInput, $jadual, $key);
-		elseif(in_array($key,dpt_senarai('jadual_biodata2') )) #senarai medan untuk biodata
-			$input = $this->inputBiodata($tab2, $tab3, $name, $data,
-				$classInput, $komenInput);
-		elseif(in_array($key,array('hasil','belanja','bilpekerja','gaji',
-			'hartatetap','stokakhir','staf','aset','stok')))
-			$input = $this->inputNumber($tab2, $tab2, $name, $data,
-				$classInput, $komenInput);
-		elseif(in_array($key,array('amt_staf','amt_output','amt_input','amt_aset','amt_gaji',
-			'amt_jualan','amt_hasil','amt_belanja')))
-			$input = $this->inputNumber($tab2, $tab2, $name, $data,
-				$classInput, $komenInput);
-		elseif ( in_array($key,array('lawat','terima','hantar','hantar_prosesan')) )
-			$input = $this->inputTarikh($tab2, $tab2, $name, $data,
-				$classInput, $komenInput, $jadual, $key);
-		elseif(in_array($key,array('no','batu','jalan','tmn_kg','daerah')))
-			$input = $this->inputAlamatBaru($tab2, $tab3, $name, $data,
-		elseif(in_array($key,array('namax','emailx','responden','fe',
-			'mko','respon','notel','nofax')))
-			$input = $this->inputTeksBesar($tab2, $tab3, $name, $data,
-				$classInput, $komenInput);
-		elseif(in_array($key,array('orang_a','notel_a','nofax_a','email_a')))
-			$input = $this->inputTeksBesar($tab2, $tab3, $name, $data,
-				$classInput, $komenInput);
-		elseif(in_array($key,array('pecah5P')))
-			$input = $this->inputTeksTakData($tab2, $tab3, $name);//*/
 		else
 		{#kod untuk lain2
 			$input = $tab2 . '<p class="form-control-static text-info">'
