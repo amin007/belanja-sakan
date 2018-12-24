5138d4a84b9f995cda0acf4f472c2be5511d15d5
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-24 14:08:50 +0800

    asingkan fail html diatas dan di bawah dalam folder atas4.1.3

--- Aplikasi/Fail/Papar/index/daftar.php
+++ Aplikasi/Fail/Papar/index/daftar.php
@@ -1,45 +1,4 @@
-<?php
-$bootstrapCSS_413 = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css';
- $ceruleanCSS_413 = 'https://maxcdn.bootstrapcdn.com/bootswatch/4.1.3/cerulean/bootstrap.min.css';
- $fontawesome_510 = 'https://use.fontawesome.com/releases/v5.1.0/css/all.css';
-?><!doctype html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="description" content="">
-<meta name="author" content="">
-<title>Belanja Sakan</title>
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<link href="<?php echo $bootstrapCSS_413 ?>" rel="stylesheet" type="text/css">
-<link href="<?php echo $fontawesome_510 ?>" rel="stylesheet" type="text/css">
-
-<style type="text/css">
-table.excel {
-	border-style:ridge;
-	border-width:1;
-	border-collapse:collapse;
-	font-family:sans-serif;
-	font-size:11px;
-}
-table.excel thead th, table.excel tbody th {
-	background:#CCCCCC;
-	border-style:ridge;
-	border-width:1;
-	text-align: center;
-	vertical-align: top;
-}
-table.excel tbody th { text-align:center; vertical-align: top; }
-table.excel tbody td { vertical-align:bottom; }
-table.excel tbody td
-{
-	padding: 0 3px; border: 1px solid #aaaaaa;
-	background:#ffffff;
-}
-.fa-input {font-family: FontAwesome}
-</style>
-</head>
-<body>
+<?php include 'atas4.1.3/diatas.php'; ?>
 
 <!-- Jadual nama_pengguna ########################################### -->
 <br><br><br>
@@ -120,23 +79,4 @@ class="form-horizontal">
 <!-- Jadual nama_pengguna ########################################### -->
 <!-- menu tengah bawah -->
 
-<!-- Footer
-================================================== -->
-<!-- footer class="footer">
-	<div class="container">
-		<span class="label label-info">
-		&copy; Hak Cipta Terperihara 2016. Theme Asal Bootstrap Twitter
-		</span>
-	</div>
-</footer -->
-
-<!-- khas untuk jquery dan js2 lain
-<?php
-      $jquery_cdn = 'https://code.jquery.com/jquery-2.2.3.min.js';
- $bootstrapJS_413 = 'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js';
-?>
-================================================== -->
-<script type="text/javascript" src="<?php echo $jquery_cdn ?>"></script>
-<script type="text/javascript" src="<?php echo $bootstrapJS_413 ?>"></script>
-</body>
-</html>
+<?php include 'atas4.1.3/dibawah.php'; ?>
