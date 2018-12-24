eb27a38f451918d251a533bd713d606a547716fd
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-24 13:19:56 +0800

    tambah fail baru

---
+++ Aplikasi/Fail/Papar/ruangtamu/admin2home.php
@@ -0,0 +1,22 @@
+<div class="container">
+<hr><h1>Admin2home - Kita Tanya Apa Khabar</h1><hr>
+<hr><h2>Untuk Projek HIES sahaja</h2><hr>
+<div class="hero-unit">
+<p><?php
+$Sesi = new \Aplikasi\Kitab\Sesi();
+$Sesi->init();
+//echo '<pre>'; print_r($_SESSION); echo '</pre>';
+echo 'namaPendek=' . $Sesi->get('bs_namaPendek') . '<br>';
+echo 'namaPenuh=' . $Sesi->get('bs_namaPenuh') . '<br>';
+echo 'email=' . $Sesi->get('bs_email') . '<br>';
+echo 'nohp=' . $Sesi->get('bs_nohp') . '<br>';
+echo 'levelPengguna=' . $Sesi->get('bs_levelPengguna') . '<br>';
+echo 'url=' . URL . '';
+//*/
+?></p>
+
+	<a class="btn btn-primary btn-large" href="<?php echo URL ?>admin2home/logout">
+	Keluar<i class="fa fa-binoculars fa-2x"></i></a>
+
+</div><!-- / class="hero-unit" -->
+</div><!-- / class="container" -->
