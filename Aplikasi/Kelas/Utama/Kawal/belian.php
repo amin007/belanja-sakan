426a9761ef42be3731d808558f7a6b6d540ac74c
Amin Ledang @ Dell <helmi007@yahoo.com>
2018-12-26 19:19:40 +0800

    tukar contoh_cariKhas03 kepada borangBaru01

--- Aplikasi/Kelas/Utama/Kawal/belian.php
+++ Aplikasi/Kelas/Utama/Kawal/belian.php
@@ -168,7 +168,7 @@ class Belian extends \Aplikasi\Kitab\Kawal
 		list($$data,$meta) = $this->tanya->pilihMedan03//cariSql
 			($jadual, $medan, $carian, $susun);
 		$this->papar->_meta = $this->ubahMeta($meta);
-		$this->papar->senarai[$jadual] = $this->tanya->contoh_cariKhas03();
+		$this->papar->senarai[$jadual] = $this->tanya->borangBaru01();
 		# Set pembolehubah untuk Papar
 		$this->kandunganPaparan($p1, $jadual);//*/
 	}
