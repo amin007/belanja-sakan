6f2e9cb7eae434d551cd4ddebcea666e276b6bdd
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-26 12:36:40 +0800

    tukar nama class

--- Aplikasi/Fail/Papar/cari/pilih_jadual_am.php
+++ Aplikasi/Fail/Papar/cari/pilih_jadual_am.php
@@ -16,7 +16,8 @@
 		}
 	# papar data $row ------------------------------------------------
 	?><tr><td align="center"><?php echo $kira+1 ?></td><?php
-		$html = new \Aplikasi\Kitab\Borang04_Jadual;
+		//$html = new \Aplikasi\Kitab\Borang04_Jadual;
+		$html = new \Aplikasi\Kitab\Html_TD;
 		foreach ( $row[$kira] as $key=>$data )
 		{
 			$html->paparURL($key, $data, $myTable,
