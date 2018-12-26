dd7fdd5cfb2f15a71c4f748e2ea0d17acb885103
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-26 13:12:03 +0800

    samakan semua kedudukan fungsi pada semua class anak2 Tanya

--- Aplikasi/Kelas/Utama/Tanya/belian_tanya.php
+++ Aplikasi/Kelas/Utama/Tanya/belian_tanya.php
@@ -21,7 +21,16 @@ class Belian_Tanya extends \Aplikasi\Kitab\Tanya
 		$data = array(
 			'namaPendek' => 'james007',
 			'namaPenuh' => 'Polan Bin Polan',
-			'level' => 'pelawat'
+			'level' => 'pelawat',
+			. ' concat_ws("|",' . "\r"
+			. ' 	concat_ws("="," hasil",format(hasil,0)),' . "\r"
+			. ' 	concat_ws("="," belanja",format(belanja,0)),' . "\r"
+			. ' 	concat_ws("="," gaji",format(gaji,0)),' . "\r"
+			. ' 	concat_ws("="," aset",format(aset,0)),' . "\r"
+			. ' 	concat_ws("="," staf",format(staf,0)),' . "\r"
+			. ' 	concat_ws("="," stok akhir",format(stok,0))' . "\r"
+			. ' ) as data5P,nota'
+
 		); # dapatkan medan terlibat
 		$kira = 1; # kira jumlah data
 
