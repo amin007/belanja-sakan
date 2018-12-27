39738f76ae1e6d9581c56265e7233cc93b56410f
Amin Ledang @ Dell <helmi007@yahoo.com>
2018-12-28 00:40:21 +0800

    cuba test template baru

--- Aplikasi/Fail/Papar/cari/b_baru.php
+++ Aplikasi/Fail/Papar/cari/b_baru.php
@@ -10,6 +10,7 @@ $pilihJadual = 'baru_medan01'; # borang baru berasaskan bootstrap
 //echo '<pre>$senarai='; print_r($this->senarai); echo '</pre>';
 //echo 'template = ' . $this->template . "\r<hr>";//*/
 include $this->template . '.php';
+include 'z-navtab.php';
 #---------------------------------------------------------------------------------------
 //echo '<pre>$bentukJadual01='; print_r($this->bentukJadual01); echo '</pre>';
 //echo '<pre>$meta='; print_r($this->_meta); echo '</pre>';
