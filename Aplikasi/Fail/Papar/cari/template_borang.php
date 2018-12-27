8ecbbd2e4330f402b56b954486e0653a02063a7b
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-27 14:47:37 +0800

    susun balik form

--- Aplikasi/Fail/Papar/cari/template_borang.php
+++ Aplikasi/Fail/Papar/cari/template_borang.php
@@ -1,17 +1,29 @@
 <!-- h1> Ini Template Borang </h1 -->
 <?php
+//$html = new Aplikasi\Kitab\Borang01_Tambah;
+$html = new Aplikasi\Kitab\BrgBaru01;
+$aksi = URL . $this->_method . '/baruSimpan/' . $this->carian[0];
+$class1 = 'col-sm-7'; # untuk tajuk dan hantar
+$class2 = 'col-sm-6 '; # untuk $data
+
+echo "\n";?><br>
+<br><span class="border border-secondary">
+<form method="POST" action="<?php echo $aksi ?>" class="form-horizontal">
+<?php echo "\n";
 foreach ($this->senarai as $myTable => $row)
 {
 	if ( count($row)==0 ) echo '';
 	else
 	{
-		$tajukjadual = '<span class="badge badge-success">' . $myTable . '</span>'
-		. "\r" . '<span class="badge">' . count($row) . '</span>';
-		echo "\n$tajukjadual"; ?>
-<!-- Jadual <?php echo $myTable ?> ########################################### -->
-<?php //include 'pilih_' . $pilihJadual . '.php'; ?>
-<!-- Jadual <?php echo $myTable ?> ########################################### -->
-<?php
-	} // if ( count($row)==0 )
+		include 'pilih_' . $pilihJadual . '.php';
+	}#if ( count($row)==0 )
 }
-?>
+echo '<hr>';
+$html->medanHantar($this->myTable, $class1);
+/*<!-- / class="container" -->*/ ?>
+</form><!-- / class="form-horizontal" -->
+</span><!-- / class="border border-secondary" --><br>
+
+<div class="spinner-border" role="status">
+	<span class="sr-only">Loading...</span>
+</div>
