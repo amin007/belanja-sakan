176887421b2d74ce6769caca93c4ddecb540c017
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-27 14:51:46 +0800

    tukar template lain

--- Aplikasi/Fail/Papar/cari/b_baru.php
+++ Aplikasi/Fail/Papar/cari/b_baru.php
@@ -2,8 +2,8 @@
 # pilih paparan ke bawah atau melintang
 //$pilihJadual = 'jadual_am';
 //$pilihJadual = 'baru_medan00'; # borang baru berasaskan table
-//$pilihJadual = 'baru_medan01'; # borang baru berasaskan bootstrap
-$pilihJadual = 'baru_medan02'; # borang ubah berasaskan bootstrap
+$pilihJadual = 'baru_medan01'; # borang baru berasaskan bootstrap
+//$pilihJadual = 'baru_medan02'; # borang ubah berasaskan bootstrap
 
 # untuk kod baru
 //echo '<pre>$carian='; print_r($this->carian); echo '</pre>';
