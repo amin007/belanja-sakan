85049a37a4bc4d95b34ae88681dabff00cfde96a
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-27 12:54:40 +0800

    tambah text-right

--- Aplikasi/Fail/Papar/cari/pilih_baru_medan02.php
+++ Aplikasi/Fail/Papar/cari/pilih_baru_medan02.php
@@ -7,14 +7,16 @@ $class2 = 'col-sm-6 '; # untuk $data
 $html->medanCarian(
 	array($this->_method, $myTable, $this->senarai, $this->cariID, $this->_jadual)
 );?>
+
+<span class="border border-secondary">
 <form method="POST" action="<?php echo $aksi ?>"
 class="form-horizontal"><?php echo "\n";
 for ($kira=0; $kira < count($row); $kira++)
 {	foreach ( $row[$kira] as $key=>$data )
 	{## papar data $row ----------------------------------------------------------
-		?><div class="form-group"><?php echo "\n\t";
-		?><label for="inputTajuk" class="col-sm-2 control-label"><?php echo $key
-		?></label><?php echo "\n\t";
+		?><div class="form-group row"><?php echo "\n\t";
+		?><label for="inputTajuk" class="col-sm-2 control-label text-right"><?php
+		echo $key ?></label><?php echo "\n\t";
 		?><div class="<?php echo $class2 ?>"><?php
 		$paparData = $html->ubahInput2($this->bentukJadual01,
 			$this->_meta, $this->_jadual,$kira, $key, $data);
@@ -25,3 +27,4 @@ for ($kira=0; $kira < count($row); $kira++)
 }$html->medanHantar($this->_jadual, $class1);
 /*<!-- / class="container" -->*/ ?>
 </form><!-- / class="form-horizontal" -->
+</span><!-- / class="border border-secondary" -->
