2e3b4a7e3c5ead10404a49a65ddfc18d552547fe
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-27 14:47:05 +0800

    asingkan form ikut table

---
+++ Aplikasi/Fail/Papar/cari/pilih_baru_medan01.php
@@ -0,0 +1,22 @@
+<?php
+$tajukjadual = '<span class="badge badge-success">' . $myTable . '</span>';
+//. "\r" . '<span class="badge">' . count($row) . '</span>';
+//echo "\n<hr>$tajukjadual";
+$html->medanCarian(
+	array($this->_method, $myTable, $this->senarai, $this->cariID, $this->_jadual)
+	);
+for ($kira=0; $kira < count($row); $kira++)
+{	foreach ( $row[$kira] as $key=>$data )
+	{## papar data $row ----------------------------------------------------------
+		?><div class="form-group row"><?php echo "\n\t";
+		?><label for="inputTajuk" class="col-sm-2 control-label text-right"><?php
+		echo $key ?></label><?php echo "\n\t";
+		?><div class="<?php echo $class2 ?>"><?php
+		$paparData = $html->ubahInput2($this->bentukJadual01,
+			$this->_meta, $myTable,$kira, $key, $data);
+		echo "\n\t\t" . $paparData . "\n\t";
+		?></div><!-- / class="<?php echo $class2 ?>" --><?php echo "\n";
+		?></div><!-- / class="form-group" --><?php echo "\n";
+	}## --------------------------------------------------------------------------
+}
+?>
