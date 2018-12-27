30c657d9eadc97384d4422cce5ada2df64068092
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-27 11:38:39 +0800

    tambah menu di atas

--- Aplikasi/Fail/Papar/template/A4.1.3/menu_atas.php
+++ Aplikasi/Fail/Papar/template/A4.1.3/menu_atas.php
@@ -19,7 +19,20 @@ $iconFA['video'] = '<i class="fas fa-video"></i>';
 $iconFA['search'] = '<i class="fas fa-search"></i>';
 
 echo "\n\n"; $paras = null;
-if ($paras == null): else: ?>
+if ($paras == null):?>
+<nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+	<a class="navbar-brand" href="<?php echo URL ?>">
+		<?php echo $iconFA['video'] . Tajuk_Muka_Surat . ':' . $paras ?></a>
+	<a class="navbar-brand" href="<?php echo URL ?>ruangtamu/logout">
+		<i class="fa fa-times fa-2x" aria-hidden="true"></i>Keluar</a>
+
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+	aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+</nav>
+<br>
+<?php else: ?>
 <nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
 	<a class="navbar-brand" href="<?php echo URL ?>">
 		<?php echo $iconFA['video'] . Tajuk_Muka_Surat . ':' . $paras ?></a>
