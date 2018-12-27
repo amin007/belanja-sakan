5a4da2a3518ccf7b53df447eaa0fe76fbbe6a20d
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-27 12:27:56 +0800

    semak data type

--- Aplikasi/Kelas/Kitab/Vendor/Borang02/BrgBaru01.php
+++ Aplikasi/Kelas/Kitab/Vendor/Borang02/BrgBaru01.php
@@ -72,12 +72,12 @@ class BrgBaru01
 	public function ubahInput2($ulangdata,$jenis,$jadual,$kira,$key,$data)
 	{	# istihar pembolehubah
 		$name = 'name="' . $jadual . '[' . $key . ']"';
-		$dataType = $jenis[$key]['type'];// myGetType($data);
+		$dataType = $jenis[$jadual][$key]['type'];// myGetType($data);
 		$semua = array($jenis,$jadual,$kira,$key,$data,$name);
 		//if ( in_array($key,array(...)) )
 		if( in_array($key,array('password','kataLaluan')) )
 			$input = $this->inputPassword($semua);
-		elseif( in_array($key,array('nohp')) )
+		/*elseif( in_array($key,array('nohp')) )
 			$input = $this->inputBiodata($semua);
 		/*elseif(in_array($dataType,array('VAR_STRING')))
 			$input = $this->inputTeksBesar($semua);
