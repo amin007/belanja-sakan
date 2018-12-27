5736c36c0d280fa43763c97cb6261ab19300f63d
Amin Ledang @ Dell <helmi007@yahoo.com>
2018-12-28 00:14:32 +0800

    enter 2 kali

--- Aplikasi/Fail/Papar/cari/template_borang.php
+++ Aplikasi/Fail/Papar/cari/template_borang.php
@@ -30,9 +30,8 @@ $aksi = URL . $this->_method . '/baruSimpan/' . $this->carian[0];
 $class1 = 'col-sm-7'; # untuk tajuk dan hantar
 $class2 = 'col-sm-6'; # untuk $data
 echo "\n";?><br>
-
 <form method="POST" action="<?php echo $aksi ?>" class="form-horizontal">
-<?php echo "\n";
+<?php
 foreach ($this->senarai as $myTable => $row)
 {
 	if ( count($row)==0 ) echo '';
@@ -45,3 +44,5 @@ echo '<hr>';
 $html->medanHantar($this->myTable, $class1);
 /*<!-- / class="container" -->*/ ?>
 </form><!-- / class="form-horizontal" -->
+
+
