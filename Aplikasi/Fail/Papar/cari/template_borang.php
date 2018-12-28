d4ec68e1a3ee67bd573450c372210f3c72a4a56a
Amin Ledang @ Dell <helmi007@yahoo.com>
2018-12-28 17:01:39 +0800

    susun balik borang

--- Aplikasi/Fail/Papar/cari/template_borang.php
+++ Aplikasi/Fail/Papar/cari/template_borang.php
@@ -6,22 +6,16 @@ $aksi = URL . $this->_method . '/baruSimpan/' . $this->carian[0];
 $class1 = 'col-sm-7'; # untuk tajuk dan hantar
 $class2 = 'col-sm-6'; # untuk $data
 #----------------------------------------------------------------------------------------------------
-if(isset($this->senarai))
-	echo "\n" . '<form method="POST" action="' . $aksi . '" class="form-horizontal">';
-#----------------------------------------------------------------------------------------------------
 foreach($this->senarai as $myTable => $row)
 {
 	if ( count($row)==0 ) echo '';
 	else
 	{
+		echo "\n" . '<form method="POST" action="' . $aksi . '" class="form-horizontal">';
 		include 'pilih_' . $pilihJadual . '.php';
+		$html->medanHantar($this->myTable, $class1);
+		echo "\n" . '</form><!-- / class="form-horizontal" -->';
 	}#if ( count($row)==0 )
-}
-#----------------------------------------------------------------------------------------------------
-if(isset($this->senarai))
-{
-	$html->medanHantar($this->myTable, $class1);
-	echo "\n" . '</form><!-- / class="form-horizontal" -->';
 }
 #----------------------------------------------------------------------------------------------------
 /*<!-- / class="container" -->*/ ?>
