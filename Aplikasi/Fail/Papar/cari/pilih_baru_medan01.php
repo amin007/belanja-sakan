ba9ec9afc0f0af6212f1608c6852496fb51bcbeb
Amin Ledang @ Dell <helmi007@yahoo.com>
2018-12-28 17:24:18 +0800

    susun balik pembolehubah yang patut dihantar dalam class lain

--- Aplikasi/Fail/Papar/cari/pilih_baru_medan01.php
+++ Aplikasi/Fail/Papar/cari/pilih_baru_medan01.php
@@ -2,23 +2,21 @@
 $tajukjadual = '<span class="badge badge-success">' . $myTable . '</span>';
 //. "\r" . '<span class="badge">' . count($row) . '</span>';
 //echo "\n<hr>$tajukjadual";
-$html->medanCarian(
-	array($this->_method, $myTable, $this->senarai, $this->cariID, $this->_jadual)
-	);
 for ($kira=0; $kira < count($row); $kira++)
 {	foreach ( $row[$kira] as $key=>$data )
 	{## papar data $row ----------------------------------------------------------
-		//$dataType = $this->_meta[$myTable][$key]['type'];
-		$class = 'col-sm-2 control-label text-right';
-	## ---------------------------------------------------------------------------?>
+		$name = 'name="' . $myTable . '[' . $key . ']"';
+		$dataType = $this->_meta[$myTable][$key]['type'];
+		$semua = array($this->_meta,$myTable,$kira,$key,$data,$name);
+		$classA = 'col-sm-2 control-label text-right';?>
 <!-- ================================================================================================= --><?php
 		echo "\n";
 		?><div class="form-group row"><?php echo "\n\t";
-		?><label for="inputTajuk" class="<?php echo $class ?>"><?php
+		?><label for="inputTajuk" class="<?php echo $classA ?>"><?php
 		echo $key ?></label><?php echo "\n\t";
 		?><div class="<?php echo $class2 ?>"><?php
-		$paparData = $html->ubahInput2($this->bentukJadual01,
-			$this->_meta, $myTable,$kira, $key, $data);
+		$paparData = $html->ubahInput2($key,$data,$dataType,
+			$semua,$this->bentukJadual01);
 		echo "\n\t\t" . $paparData . "\n\t";
 		?></div><!-- / class="<?php echo $class2 ?>" --><?php echo "\n";
 		?></div><!-- / class="form-group" --><?php echo "\n";
