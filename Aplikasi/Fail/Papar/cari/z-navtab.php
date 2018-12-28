df587e7ae789703d376c4d819ced97aab0a3eafa
Amin Ledang @ Dell <helmi007@yahoo.com>
2018-12-28 16:34:52 +0800

    cuba fahami macam mana menu tab dalam bootstrap yang baru berfungsi

--- Aplikasi/Fail/Papar/cari/z-navtab.php
+++ Aplikasi/Fail/Papar/cari/z-navtab.php
@@ -1,19 +1,19 @@
 <div class="row">
-<div class="col-3">
-	<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-	<a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-	<a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-	<a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-	<a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
-	</div>
-</div>
-<div class="col-9">
-	<div class="tab-content" id="v-pills-tabContent">
-		<div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">A</div>
-		<div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+<div class="col-2"><div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+	<a class="nav-link" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home"
+	role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+	<a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile"
+	role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+</div></div>
+<div class="col-10"><div class="tab-content" id="v-pills-tabContent">
+<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+	<div class="tab-pane fade" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">A</div>
+	<div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
 <!-- ================================================================================================= -->
 <div class="form-group row">
-	<label for="inputTajuk" class="col-sm-2 control-label text-right">jantina</label>
+	<label for="inputTajuk" class="col-sm-2 control-label">jantina</label>
 	<div class="col-sm-6">
 		<div class="input-group input-group-sm">
 		<select name="senarai_isirumah1[jantina]" class="form-control">
@@ -24,10 +24,7 @@
 	</div><!-- / class="col-sm-6" -->
 </div><!-- / class="form-group" -->
 <!-- ================================================================================================= -->		
-		
 		</div>
-		<div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">C</div>
-		<div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">D</div>
-	</div>
-</div>
+<!-- @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+</div></div>
 </div>
