37f898435c070ba8da1a5854dd798febe73efea4
Amin Ledang @ Dell <helmi007@yahoo.com>
2018-12-28 17:42:51 +0800

    pindah ke dalam foreach

--- Aplikasi/Fail/Papar/cari/template_borang.php
+++ Aplikasi/Fail/Papar/cari/template_borang.php
@@ -5,21 +5,21 @@ $html = new Aplikasi\Kitab\BrgBaru01;
 $aksi = URL . $this->_method . '/baruSimpan/' . $this->carian[0];
 $class1 = 'col-sm-7'; # untuk tajuk dan hantar
 $class2 = 'col-sm-6'; # untuk $data
-#----------------------------------------------------------------------------------------------------
-//$html->medanCarian(
-//	array($this->_method, $myTable, $this->senarai, $this->cariID, $this->_jadual)
-//	);
-#----------------------------------------------------------------------------------------------------
+
 foreach($this->senarai as $myTable => $row)
 {
 	if ( count($row)==0 ) echo '';
 	else
 	{
+#----------------------------------------------------------------------------------------------------
+		$html->medanCarian(
+			array($this->_method, $myTable, $this->senarai, $this->cariID, $this->_jadual)
+			);
 		echo "\n" . '<form method="POST" action="' . $aksi . '" class="form-horizontal">';
 		include 'pilih_' . $pilihJadual . '.php';
 		$html->medanHantar($this->myTable, $class1);
 		echo "\n" . '</form><!-- / class="form-horizontal" -->';
+#----------------------------------------------------------------------------------------------------
 	}#if ( count($row)==0 )
 }
-#----------------------------------------------------------------------------------------------------
 /*<!-- / class="container" -->*/ ?>
