116046f581477502e71632ec069520fcbfe776bf
Amin Ledang @ Dell <helmi007@yahoo.com>
2018-12-28 16:35:30 +0800

    buat contoh koding di luar sistem

---
+++ sumber/fail/php/cth/bootstrap/contoh01.php
@@ -0,0 +1,46 @@
+<?php include 'diatas4.13/diatas.php'; ?>
+
+<div class="row">
+<div class="col-2">
+	<div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+	<a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+	<a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+	</div>
+</div>
+<div class="col-10">
+	<div class="tab-content" id="v-pills-tabContent">
+		<div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">A</div>
+		<div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
+<!-- ================================================================================================= -->
+<div class="form-group row">
+	<label for="inputTajuk" class="col-sm-2 control-label">jantina</label>
+	<div class="col-sm-6">
+		<div class="input-group input-group-sm">
+		<select name="senarai_isirumah1[jantina]" class="form-control">
+			<option value="1">1-Lelaki</option>
+			<option value="2">2-Perempuan</option>
+			</select>
+		</div>
+	</div><!-- / class="col-sm-6" -->
+</div><!-- / class="form-group" -->
+<!-- ================================================================================================= -->		
+		</div>
+	</div>
+</div>
+</div>
+
+<nav><div class="nav nav-tabs" id="nav-tab" role="tablist">
+	<a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home"
+	role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+	<a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile"
+	role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
+	<a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact"
+	role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
+</div></nav>
+<div class="tab-content" id="nav-tabContent">
+	<div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
+	<div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+	<div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">...</div>
+</div>
+
+<?php include 'diatas4.13/diatas.php'; ?>
