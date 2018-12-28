5764cb93101907cd4ed354c5dc04f8009b6960ad
Amin Ledang @ Dell <helmi007@yahoo.com>
2018-12-28 16:33:34 +0800

    buang koding yang tidak penting

--- Aplikasi/Fail/Papar/cari/template_borang.php
+++ Aplikasi/Fail/Papar/cari/template_borang.php
@@ -1,27 +1,3 @@
-<div class="spinner-border text-primary" role="status">
-	<span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-secondary" role="status">
-	<span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-success" role="status">
-	<span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-danger" role="status">
-	<span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-warning" role="status">
-	<span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-info" role="status">
-	<span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-light" role="status">
-	<span class="sr-only">Loading...</span>
-</div>
-<div class="spinner-border text-dark" role="status">
-	<span class="sr-only">Loading...</span>
-</div>
 <!-- h1> Ini Template Borang </h1 -->
 <?php
 //$html = new Aplikasi\Kitab\Borang01_Tambah;
