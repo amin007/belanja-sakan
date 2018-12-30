7ae55411d3a0747fd8c40b791be5c167be61b519
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-30 11:46:56 +0800

    susun balik koding

--- Aplikasi/Fail/Papar/cari/pilih_ubah_medan02.php
+++ Aplikasi/Fail/Papar/cari/pilih_ubah_medan02.php
@@ -3,16 +3,15 @@ for ($kira=0; $kira < count($row); $kira++)
 {	foreach ( $row[$kira] as $key=>$data )
 	{## papar data $row ----------------------------------------------------------
 		$name = 'name="' . $myTable . '[' . $key . ']"';
-		$dataType = $this->_meta[$key]['type'];
+		$dataType = $this->_meta[$myTable][$key]['type'];
 		$semua = array($this->_meta,$myTable,$kira,$key,$data,$name);
 		$classA = 'col-sm-2 control-label text-right';?>
 <!-- ================================================================================================= --><?php
 		echo "\n";
 		?><div class="form-group row"><?php echo "\n\t";
-		?><label for="inputTajuk" class="<?php echo $classTajuk ?>"><?php
+		?><label for="inputTajuk" class="<?php echo $classA ?>"><?php
 		echo $key ?></label><?php echo "\n\t";
 		?><div class="<?php echo $class2 ?>"><?php
-		//$paparData = contohInput($data);
 		$paparData = $html->ubahInput2($key,$data,$dataType,
 			$semua,$this->bentukJadual01);
 		echo "\n\t\t" . $paparData . "\n\t";
@@ -20,6 +19,7 @@ for ($kira=0; $kira < count($row); $kira++)
 		?></div><!-- / class="form-group" --><?php echo "\n";
 	}## --------------------------------------------------------------------------
 }
+?><!-- ================================================================================================= --><?php
 
 function contohInput($data)
 {
