e593003424500693cb4e1144da8d930667eabb7a
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-30 11:30:30 +0800

    tambah contoh papar input berasaskan bootstrap

--- Aplikasi/Fail/Papar/cari/pilih_ubah_medan02.php
+++ Aplikasi/Fail/Papar/cari/pilih_ubah_medan02.php
@@ -6,8 +6,17 @@ for ($kira=0; $kira < count($row); $kira++)
 		?><label for="inputTajuk" class="col-sm-2 control-label"><?php echo $key
 		?></label><?php echo "\n\t";
 		?><div class="<?php echo $class2 ?>"><?php
-		echo "\n\t\t" . $data . "\n\t";
+		$paparData = contohInput($data);
+		echo "\n\t\t" . $paparData . "\n\t";
 		?></div><!-- / class="<?php echo $class2 ?>" --><?php echo "\n";
 		?></div><!-- / class="form-group" --><?php echo "\n";
 	}## --------------------------------------------------------------------------
+}
+
+function contohInput($data)
+{
+	$input2 = '<p class="form-control-static text-info">'
+			. $data . '</p>';
+
+	return $input2;
 }
