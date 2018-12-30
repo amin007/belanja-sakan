f4ebf10f6c955742bf26fe9e61edfb5f479661f6
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2018-12-30 11:36:37 +0800

    susun balik koding untuk css tajuk medan

--- Aplikasi/Fail/Papar/cari/pilih_ubah_medan02.php
+++ Aplikasi/Fail/Papar/cari/pilih_ubah_medan02.php
@@ -1,10 +1,11 @@
 <?php echo "\n";
+$classTajuk = 'col-sm-2 control-label text-right';
 for ($kira=0; $kira < count($row); $kira++)
 {	foreach ( $row[$kira] as $key=>$data )
 	{## papar data $row ----------------------------------------------------------
-		?><div class="form-group"><?php echo "\n\t";
-		?><label for="inputTajuk" class="col-sm-2 control-label"><?php echo $key
-		?></label><?php echo "\n\t";
+		?><div class="form-group row"><?php echo "\n\t";
+		?><label for="inputTajuk" class="<?php echo $classTajuk ?>"><?php
+		echo $key ?></label><?php echo "\n\t";
 		?><div class="<?php echo $class2 ?>"><?php
 		$paparData = contohInput($data);
 		echo "\n\t\t" . $paparData . "\n\t";
