1263747ab26072b34002086a946d91d0d454a1a8
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-01-03 11:50:40 +0800

    tambah contoh tatasusunan

---
+++ sumber/fail/php/cth/tatasusunan/contoh01.php
@@ -0,0 +1,408 @@
+<?php
+# contoh dari database
+$c2=array (
+  'senarai_belanja' => 
+  array (
+    0 => 
+    array (
+      'medan' => 'nilai_sebenar',
+      'kod' => '1',
+      'keterangan' => 'sebenar',
+    ),
+    1 => 
+    array (
+      'medan' => 'nilai_sebenar',
+      'kod' => '2',
+      'keterangan' => 'anggaran',
+    ),
+    2 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '01',
+      'keterangan' => 'pasar basah',
+    ),
+    3 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '02',
+      'keterangan' => 'pasar borong',
+    ),
+    4 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '03',
+      'keterangan' => 'pasar tani/pasar tamu',
+    ),
+    5 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '04',
+      'keterangan' => 'pasar malam',
+    ),
+    6 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '05',
+      'keterangan' => 'kedai runcit di dalam pasar basah/pasar borong',
+    ),
+    7 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '06',
+      'keterangan' => 'kedai runcit',
+    ),
+    8 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '07',
+      'keterangan' => 'kedai runcit/akhbar di pusat membeli belah',
+    ),
+    9 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '08',
+      'keterangan' => 'pasar mini / mini market',
+    ),
+    10 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '09',
+      'keterangan' => 'Pasar raya',
+    ),
+    11 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '10',
+      'keterangan' => 'departmental store',
+    ),
+    12 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '11',
+      'keterangan' => 'Kedai `convenience`',
+    ),
+    13 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '12',
+      'keterangan' => 'Pasar raya besar (Hypermarket)',
+    ),
+    14 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '13',
+      'keterangan' => 'kedai khusus',
+    ),
+    15 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '14',
+      'keterangan' => 'restoran/kedai makan',
+    ),
+    16 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '15',
+      'keterangan' => 'restoran bercawangan / food court',
+    ),
+    17 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '16',
+      'keterangan' => 'gerai kecil/karavan/ food truck/kiosk',
+    ),
+    18 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '17',
+      'keterangan' => 'restoran berhawa dingin / restoran 24 jam',
+    ),
+    19 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '18',
+      'keterangan' => 'stesen petrol',
+    ),
+    20 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '19',
+      'keterangan' => 'farmasi',
+    ),
+    21 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '20',
+      'keterangan' => 'pembelian atas talian/pembelian melalui tempahan',
+    ),
+    22 => 
+    array (
+      'medan' => 'punca',
+      'kod' => '21',
+      'keterangan' => 'lain-lain',
+    ),
+    23 => 
+    array (
+      'medan' => 'tukaran',
+      'kod' => '01',
+      'keterangan' => 'bayaran terus | direct payment',
+    ),
+    24 => 
+    array (
+      'medan' => 'tukaran',
+      'kod' => '02',
+      'keterangan' => 'cek/bank draf/kiriman wang/wang pos |check / bank draft / remittance / postal order',
+    ),
+    25 => 
+    array (
+      'medan' => 'tukaran',
+      'kod' => '03',
+      'keterangan' => 'kad atm/debit/kad prabayar(contoh: tng) |atm / debit card / prepaid card (e.g: tng)',
+    ),
+    26 => 
+    array (
+      'medan' => 'tukaran',
+      'kod' => '04',
+      'keterangan' => 'perbankan melalui internet/telefon |banking via internet / phone',
+    ),
+    27 => 
+    array (
+      'medan' => 'tukaran',
+      'kod' => '05',
+      'keterangan' => 'pembayaran melalui telefon |payment by phone',
+    ),
+    28 => 
+    array (
+      'medan' => 'tukaran',
+      'kod' => '06',
+      'keterangan' => 'kad kredit/kad caj |credit card / charge card',
+    ),
+    29 => 
+    array (
+      'medan' => 'tukaran',
+      'kod' => '07',
+      'keterangan' => 'kredit selain dari menggunakan kad kredit |credit other than using a credit card',
+    ),
+    30 => 
+    array (
+      'medan' => 'tukaran',
+      'kod' => '08',
+      'keterangan' => 'sewa beli | hire purchase',
+    ),
+    31 => 
+    array (
+      'medan' => 'tukaran',
+      'kod' => '09',
+      'keterangan' => 'percuma | free',
+    ),
+    32 => 
+    array (
+      'medan' => 'tukaran',
+      'kod' => '10',
+      'keterangan' => 'lain-lain (contoh:konsesi) |others (eg: concessions)',
+    ),
+    33 => 
+    array (
+      'medan' => 'edagang',
+      'kod' => '1',
+      'keterangan' => 'ya',
+    ),
+    34 => 
+    array (
+      'medan' => 'edagang',
+      'kod' => '2',
+      'keterangan' => 'tidak',
+    ),
+  ),
+);
+
+# ubahsuai sikit, rujuk class public Belian_Tanya->cariKhas01($medan1)
+$c1=array (
+  'nilai_sebenar' => 
+  array (
+    0 => 
+    array (
+      'kod' => '1',
+      'keterangan' => 'sebenar',
+    ),
+    1 => 
+    array (
+      'kod' => '2',
+      'keterangan' => 'anggaran',
+    ),
+  ),
+  'punca' => 
+  array (
+    2 => 
+    array (
+      'kod' => '01',
+      'keterangan' => 'pasar basah',
+    ),
+    3 => 
+    array (
+      'kod' => '02',
+      'keterangan' => 'pasar borong',
+    ),
+    4 => 
+    array (
+      'kod' => '03',
+      'keterangan' => 'pasar tani/pasar tamu',
+    ),
+    5 => 
+    array (
+      'kod' => '04',
+      'keterangan' => 'pasar malam',
+    ),
+    6 => 
+    array (
+      'kod' => '05',
+      'keterangan' => 'kedai runcit di dalam pasar basah/pasar borong',
+    ),
+    7 => 
+    array (
+      'kod' => '06',
+      'keterangan' => 'kedai runcit',
+    ),
+    8 => 
+    array (
+      'kod' => '07',
+      'keterangan' => 'kedai runcit/akhbar di pusat membeli belah',
+    ),
+    9 => 
+    array (
+      'kod' => '08',
+      'keterangan' => 'pasar mini / mini market',
+    ),
+    10 => 
+    array (
+      'kod' => '09',
+      'keterangan' => 'Pasar raya',
+    ),
+    11 => 
+    array (
+      'kod' => '10',
+      'keterangan' => 'departmental store',
+    ),
+    12 => 
+    array (
+      'kod' => '11',
+      'keterangan' => 'Kedai `convenience`',
+    ),
+    13 => 
+    array (
+      'kod' => '12',
+      'keterangan' => 'Pasar raya besar (Hypermarket)',
+    ),
+    14 => 
+    array (
+      'kod' => '13',
+      'keterangan' => 'kedai khusus',
+    ),
+    15 => 
+    array (
+      'kod' => '14',
+      'keterangan' => 'restoran/kedai makan',
+    ),
+    16 => 
+    array (
+      'kod' => '15',
+      'keterangan' => 'restoran bercawangan / food court',
+    ),
+    17 => 
+    array (
+      'kod' => '16',
+      'keterangan' => 'gerai kecil/karavan/ food truck/kiosk',
+    ),
+    18 => 
+    array (
+      'kod' => '17',
+      'keterangan' => 'restoran berhawa dingin / restoran 24 jam',
+    ),
+    19 => 
+    array (
+      'kod' => '18',
+      'keterangan' => 'stesen petrol',
+    ),
+    20 => 
+    array (
+      'kod' => '19',
+      'keterangan' => 'farmasi',
+    ),
+    21 => 
+    array (
+      'kod' => '20',
+      'keterangan' => 'pembelian atas talian/pembelian melalui tempahan',
+    ),
+    22 => 
+    array (
+      'kod' => '21',
+      'keterangan' => 'lain-lain',
+    ),
+  ),
+  'tukaran' => 
+  array (
+    23 => 
+    array (
+      'kod' => '01',
+      'keterangan' => 'bayaran terus | direct payment',
+    ),
+    24 => 
+    array (
+      'kod' => '02',
+      'keterangan' => 'cek/bank draf/kiriman wang/wang pos |check / bank draft / remittance / postal order',
+    ),
+    25 => 
+    array (
+      'kod' => '03',
+      'keterangan' => 'kad atm/debit/kad prabayar(contoh: tng) |atm / debit card / prepaid card (e.g: tng)',
+    ),
+    26 => 
+    array (
+      'kod' => '04',
+      'keterangan' => 'perbankan melalui internet/telefon |banking via internet / phone',
+    ),
+    27 => 
+    array (
+      'kod' => '05',
+      'keterangan' => 'pembayaran melalui telefon |payment by phone',
+    ),
+    28 => 
+    array (
+      'kod' => '06',
+      'keterangan' => 'kad kredit/kad caj |credit card / charge card',
+    ),
+    29 => 
+    array (
+      'kod' => '07',
+      'keterangan' => 'kredit selain dari menggunakan kad kredit |credit other than using a credit card',
+    ),
+    30 => 
+    array (
+      'kod' => '08',
+      'keterangan' => 'sewa beli | hire purchase',
+    ),
+    31 => 
+    array (
+      'kod' => '09',
+      'keterangan' => 'percuma | free',
+    ),
+    32 => 
+    array (
+      'kod' => '10',
+      'keterangan' => 'lain-lain (contoh:konsesi) |others (eg: concessions)',
+    ),
+  ),
+  'edagang' => 
+  array (
+    33 => 
+    array (
+      'kod' => '1',
+      'keterangan' => 'ya',
+    ),
+    34 => 
+    array (
+      'kod' => '2',
+      'keterangan' => 'tidak',
+    ),
+  ),
+);
