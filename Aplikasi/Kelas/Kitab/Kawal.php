bc84dd464d3e54f8d69a72bad3e53a56d8c2ded9
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-01-03 13:21:41 +0800

    tukar semakData kepada semakPembolehubah

--- Aplikasi/Kelas/Kitab/Kawal.php
+++ Aplikasi/Kelas/Kitab/Kawal.php
@@ -39,7 +39,7 @@ class Kawal
 		if($p == '0') print_r($senarai);
 		if($p == '1') var_export($senarai);
 		echo '</pre>';//*/
-		//$this->semakData($ujian,'ujian',0);
+		//$this->semakPembolehubah($ujian,'ujian',0);
 		#http://php.net/manual/en/function.var-export.php
 		#http://php.net/manual/en/function.print-r.php
 	}
