ecda9cb824467eea59569e9108230f828f6cf056
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-01-03 13:05:26 +0800

    guna koding yang standard untuk semak pembolehubah

--- Aplikasi/Kelas/Utama/Kawal/belian.php
+++ Aplikasi/Kelas/Utama/Kawal/belian.php
@@ -42,11 +42,15 @@ class Belian extends \Aplikasi\Kitab\Kawal
 		//*/
 	}
 ##------------------------------------------------------------------------------------------
-	public function semakPembolehubah($senarai,$jadual)
+	public function semakPembolehubah($senarai,$jadual,$p='0')
 	{
-		echo '<pre>$jadual = ' . $jadual . '|<br>';
-		print_r($senarai); echo '</pre>';//*/
-		//$this->semakPembolehubah($ujian,'ujian');
+		echo '<pre>$' . $jadual . '=><br>';
+		if($p == '0') print_r($senarai);
+		if($p == '1') var_export($senarai);
+		echo '</pre>';//*/
+		//$this->semakData($ujian,'ujian',0);
+		#http://php.net/manual/en/function.var-export.php
+		#http://php.net/manual/en/function.print-r.php
 	}
 ##------------------------------------------------------------------------------------------
 	function logout()
