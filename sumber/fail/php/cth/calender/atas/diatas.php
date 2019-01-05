68ed0ea90f28a330b90398f1b1396f52e2713985
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-01-05 13:16:42 +0800

    buang space

--- sumber/fail/php/cth/calender/atas/diatas.php
+++ sumber/fail/php/cth/calender/atas/diatas.php
@@ -35,14 +35,14 @@ table.excel tbody td
 .fa-input {font-family: FontAwesome}
 </style>
 <style>
-  .calendar {width:220px;background:#fdfdfd;font:14px arial; color:#644; border:1px solid #dcdcdc; border-collapse:collapse;}
-  .month-heading {text-align:center; height:28px; line-height:28px; background:#b53934; color: #fff;}
-  .big {font-size:120%;}
-  .week-heading {font-size:11px; height:20px; line-height:20px; background:#e6e7ec; text-align:center;}
-  td {width:14.2857%;text-align:right;padding:4px!important; border:1px solid #dcdcdc;}
-  .other-month {color: #dcdcdc;}
-  .weekend-day {background:#fafafa;}
-  .today {background:#a6daf0;}
+.calendar {width:220px;background:#fdfdfd;font:14px arial; color:#644; border:1px solid #dcdcdc; border-collapse:collapse;}
+.month-heading {text-align:center; height:28px; line-height:28px; background:#b53934; color: #fff;}
+.big {font-size:120%;}
+.week-heading {font-size:11px; height:20px; line-height:20px; background:#e6e7ec; text-align:center;}
+td {width:14.2857%;text-align:right;padding:4px!important; border:1px solid #dcdcdc;}
+.other-month {color: #dcdcdc;}
+.weekend-day {background:#fafafa;}
+.today {background:#a6daf0;}
 </style>
 </head>
 <body>
