18aa28f65a5c0dd1c3b350851744c5d434749fdb
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-01-05 20:28:49 +0800

    susun koding ikut html

--- sumber/fail/php/cth/calender/contoh02.php
+++ sumber/fail/php/cth/calender/contoh02.php
@@ -11,9 +11,13 @@ echo '<br>(12 / 3) = ' . (12 / 3) . '|(12 % 3) = ' . (12 % 3) . "\n";
 $tahun = 2019;
 $setahun = array(1,2,3,4,5,6,7,8,9,10,11,12);
 foreach($setahun as $bulan):
-	echo "\n\t<td text-center>" . showmonth($bulan,$tahun) . '</td>';
+	echo "\n\t<td class=\"text-center\">"
+	. "\n\t<!-- *********************************************************************** -->\n\t"
+	. showmonth($bulan,$tahun)
+	. "\n\t<!-- *********************************************************************** -->\n\t"
+	. '</td>';
 	//echo "\n\t<td text-center>Bulan " . $bulan . '</td>';
-	echo ($bulan % 3 == '0') ? "</tr>\n<tr>\n" : "";
+	echo ($bulan % 3 == '0') ? "\n</tr>\n<tr>" : "";
 endforeach;
 ?></tr>
 </table>
