4c355741667d00a64821e0457992d23a43a4dd1a
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-01-06 10:15:15 +0800

    susun jadual ikut kodt html5

--- sumber/fail/php/cth/calender/contoh02.php
+++ sumber/fail/php/cth/calender/contoh02.php
@@ -1,25 +1,29 @@
 <?php include 'atas/diatas.php'; ?>
-<br>
 <?php include 'fungsi_kalender.php';
-echo  '<br>(3 / 3) = ' . (3 / 3) . ' |(3 % 3) = ' . (3 % 3) . "\n";
+$tarikh['harian'] = date('d');
+$tarikh['bulanan'] = date('m');
+$tarikh['tahunan'] = date('Y');
+//echo '$harian=' . $tarikh['harian'] . '|$bulanan=' . $tarikh['bulanan'] . '|$tahunan=' . $tarikh['tahunan'];
+/*echo  '<br>(3 / 3) = ' . (3 / 3) . ' |(3 % 3) = ' . (3 % 3) . "\n";
 echo  '<br>(6 / 3) = ' . (6 / 3) . ' |(6 % 3) = ' . (6 % 3) . "\n";
 echo  '<br>(9 / 3) = ' . (9 / 3) . ' |(9 % 3) = ' . (9 % 3) . "\n";
 echo '<br>(12 / 3) = ' . (12 / 3) . '|(12 % 3) = ' . (12 % 3) . "\n";
 //*/?>
-<table class="table">
+<div align="center">
+<table>
 <tr><?php
 $tahun = 2019;
 $setahun = array(1,2,3,4,5,6,7,8,9,10,11,12);
 foreach($setahun as $bulan):
-	echo "\n\t<td class=\"text-center\">"
+	echo "\n\t<td class=\"align-top\">"
 	. "\n\t<!-- *********************************************************************** -->\n\t"
-	. showmonth($bulan,$tahun)
+	. showmonth($bulan,$tahun,$tarikh)
 	. "\n\t<!-- *********************************************************************** -->\n\t"
-	. '</td>';
+	. "</td>\n\t<td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>";
 	//echo "\n\t<td text-center>Bulan " . $bulan . '</td>';
 	echo ($bulan % 3 == '0') ? "\n</tr>\n<tr>" : "";
 endforeach;
 ?></tr>
-</table>
+</table></div><!-- align="center" -->
 
 <?php include 'atas/dibawah.php'; ?>
