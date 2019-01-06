9477252f06d6c8f01e937fffac64dccfe8826d25
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-01-06 11:25:51 +0800

    ubahsuai css

--- sumber/fail/php/cth/calender/atas/diatas.php
+++ sumber/fail/php/cth/calender/atas/diatas.php
@@ -16,21 +16,23 @@ table.excel {
 	border-width:1;
 	border-collapse:collapse;
 	font-family:sans-serif;
-	font-size:11px;
+	font-size:15px;
 }
 table.excel thead th, table.excel tbody th {
 	background:#CCCCCC;
 	border-style:ridge;
 	border-width:1;
 	text-align: center;
+	font-size:20px;
 	vertical-align: top;
 }
 table.excel tbody th { text-align:center; vertical-align: top; }
-table.excel tbody td { vertical-align:bottom; }
 table.excel tbody td
 {
-	padding: 0 3px; border: 1px solid #aaaaaa;
+	padding: 14px;/*padding: 0 3px; */
+	border: 2px solid #000000;/*border: 1px solid #aaaaaa;*/
 	background:#ffffff;
+	vertical-align:bottom;
 }
 .fa-input {font-family: FontAwesome}
 </style>
