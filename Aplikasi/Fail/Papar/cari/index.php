8b55ca0c2e0d688b7b040374992c2c4393b3f98c
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-01-08 08:31:20 +0800

    komen

--- Aplikasi/Fail/Papar/cari/index.php
+++ Aplikasi/Fail/Papar/cari/index.php
@@ -26,6 +26,6 @@ echo '<a class="btn btn-primary" href="' . URL . $this->baruBorang . '">Tambah B
 #http://php.net/manual/en/function.var-export.php
 //$this->semakData($this->bentukJadual02,'bentukJadual02',0);
 //$this->semakData($this->c1,'c1',0);
-$this->semakData($this->c2,'c2',0);
+//$this->semakData($this->c2,'c2',0);
 //$this->semakData($this->_meta,'_meta');
 //$this->semakData($this->senarai,'senarai');
