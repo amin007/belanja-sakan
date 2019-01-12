80c8cd913ba7cbf066b57d23ce1d01f66cab2b77
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-01-12 08:57:45 +0800

    tambah jenis contoh hasilkan contoh

--- Aplikasi/Fail/Papar/caripdf/contoh001.php
+++ Aplikasi/Fail/Papar/caripdf/contoh001.php
@@ -81,7 +81,7 @@ $pdf->AddPage();
 
 # set text shadow effect
 $pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
-
+#---------------------------------------------------------------------------------------------------
 # Set some content to print
 $html = <<<EOD
 <h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
@@ -93,9 +93,7 @@ EOD;
 
 # Print text using writeHTMLCell()
 $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
-
 #---------------------------------------------------------------------------------------------------
-
 # Close and output PDF document
 # This method has several options, check the source code documentation for more information.
 $pdf->Output('example_001.pdf', 'I');
