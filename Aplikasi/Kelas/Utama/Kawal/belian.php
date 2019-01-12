697102db4ab83377a3256b143da7a3b403e28961
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-01-12 08:30:16 +0800

    setkan nama class, macam banyak sangat pula huhuhu

--- Aplikasi/Kelas/Utama/Kawal/belian.php
+++ Aplikasi/Kelas/Utama/Kawal/belian.php
@@ -97,10 +97,11 @@ class Belian extends \Aplikasi\Kitab\Kawal
 		$this->papar->carian[] = 'semua';
 		if(!isset($this->papar->c1))
 			$this->papar->c1 = null;
-		$this->papar->c2 = 'belian';
+		$this->papar->c2 = $this->_folder;
 		$this->papar->_pilih = $p1;
-		$this->papar->_method = 'belian';
-		$this->papar->baruBorang = 'belian/baru';
+		$this->papar->_method = $this->_folder;
+		$this->papar->baruBorang = $this->_folder . '/baru';
+		$this->papar->pdfBorang = $this->_folder . '/caripdf';
 		$this->papar->cariID = 'papar';
 		//$this->papar->template = 'template_biasa';
 		$this->papar->pilihJadual = 'pilih_jadual_am';
