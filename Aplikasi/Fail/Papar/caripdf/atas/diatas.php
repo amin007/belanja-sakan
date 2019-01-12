fbe5163dd8448ab884e1d3a35dea80aaf8722739
Amin Ledang @ Dell <helmi007@yahoo.com>
2019-01-12 10:31:11 +0800

    khas untuk A4 LANDSCAPE

---
+++ Aplikasi/Fail/Papar/caripdf/atas/diatas.php
@@ -0,0 +1,38 @@
+<?php
+# Include the main TCPDF library (search for installation path).
+//require_once('tcpdf_include.php');
+#---------------------------------------------------------------------------------------------------
+# create new PDF document
+//$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+/*
+echo '<br>PDF_PAGE_ORIENTATION = ' . PDF_PAGE_ORIENTATION;
+echo '<br>PDF_UNIT = ' . PDF_UNIT;
+echo '<br>PDF_PAGE_FORMAT = ' . PDF_PAGE_FORMAT;
+*/
+
+# set document information
+$pdf->SetCreator(PDF_CREATOR);
+$pdf->SetAuthor('Amin Ledang');
+$pdf->SetTitle('Contoh Asas');
+$pdf->SetSubject('TCPDF Tutorial');
+$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+# remove default header/footer
+$pdf->setPrintHeader(false);
+$pdf->setPrintFooter(false);
+
+# set default monospaced font
+$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+# set margins
+$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+
+# set auto page breaks
+$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+# set image scale factor
+$pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+#---------------------------------------------------------------------------------------------------
+$pdf->SetFont('times', 'BI', 20);# set font
+//$pdf->AddPage();# add a page
+$pdf->AddPage('L', 'A4');# A4 LANDSCAPE
---
+++ Aplikasi/Fail/Papar/caripdf/contoh000.php
@@ -0,0 +1,77 @@
+<?php
+#---------------------------------------------------------------------------------------------------
+# File name : contoh000.php
+# original file : example_002.php
+# Description : Contoh 000 for TCPDF class
+#               Letak pembolehubah yang paling asas/simple
+# Author: Nicola Asuni
+# (c) Copyright:
+# Nicola Asuni
+# Tecnick.com LTD
+# www.tecnick.com
+# info@tecnick.com
+/*
+ * Creates an example PDF TEST document using TCPDF
+ * @package com.tecnick.tcpdf
+ * @abstract TCPDF - Example: Removing Header and Footer
+ * @author Nicola Asuni
+ * @since 2008-03-04
+ **/
+#---------------------------------------------------------------------------------------------------
+# create new PDF document
+$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+require 'atas/diatas.php';
+#---------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------------
+$jadual = <<<EOD
+<style type="text/css">
+table.excel {
+	border-style:ridge;
+	border-width:1;
+	border-collapse:collapse;
+	font-family:sans-serif;
+	font-size:11px;
+}
+table.excel thead th, table.excel tbody th {
+	background:#CCCCCC;
+	border-style:ridge;
+	border-width:1;
+	text-align: center;
+	vertical-align: top;
+}
+table.excel tbody th { text-align:center; vertical-align: top; }
+table.excel tbody td { vertical-align:bottom; }
+table.excel tbody td
+{
+	padding: 0 3px; border: 1px solid #aaaaaa;
+	background:#ffffff;
+}
+.fa-input {font-family: FontAwesome}
+</style>
+<br>
+<table class="excel" border="1" cellspacing="0" cellpadding="1">
+<tr>
+	<td>COL 1 - ROW 1</td>
+	<td>COL 2 - ROW 1</td>
+	<td>COL 3 - ROW 1</td>
+</tr>
+<tr>
+	<td>COL 1 - ROW 1</td>
+	<td>COL 2 - ROW 1</td>
+	<td>COL 3 - ROW 1</td>
+</tr>
+<tr>
+	<td>COL 1 - ROW 1</td>
+	<td>COL 2 - ROW 1</td>
+	<td>COL 3 - ROW 1</td>
+</tr>
+</table>
+EOD;
+
+$pdf->writeHTML($jadual, true, false, false, false, '');
+#---------------------------------------------------------------------------------------------------
+# Close and output PDF document
+$pdf->Output('contoh000.pdf', 'I');//*/
+#===================================================================================================
+# END OF FILE
+#===================================================================================================
