71c41a636c4ea7938f1b2a0f88709f6925d6372f
Amin007 @ JPMUAR07-PC <al.amin007@yahoo.com>
2019-01-22 08:30:03 +0800

    tukar L kepada I

--- sumber/fail/php/cth/wordpress/contoh000.php
+++ sumber/fail/php/cth/wordpress/contoh000.php
@@ -102,8 +102,8 @@ bab 3: menu setup
 <?php
 function biarlah_script_enqueue()
 {
-	wp_enqueue_style('customstyle', get_template_directory_url() . '/css/biarlah.css', array(), '1.0.0', 'all');
-	wp_enqueue_script('customjs', get_template_directory_url() . '/css/biarlah.js',, array(), '1.0.0', true);# true -> letak js di bawah
+	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/biarlah.css', array(), '1.0.0', 'all');
+	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/biarlah.js',, array(), '1.0.0', true);# true -> letak js di bawah
 }
 
 add_action('wp_enqueue_scripts','biarlah_script_enqueue');
